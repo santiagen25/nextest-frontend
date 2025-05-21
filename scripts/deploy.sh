9eabc30e9ba0030db53beb5922500ec50e904aee
Santiagen25 <santiago.torrabadella@hotmail.com>
2025-05-21 07:42:30 +0200

    files ready for deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "🚀 Haciendo merge de 'dev' a 'master' y haciendo push..."
+
+git checkout master || exit 1
+git merge dev || exit 1
+git push origin master || exit 1
+
+echo "✅ ¡Deploy lanzado con éxito!"
